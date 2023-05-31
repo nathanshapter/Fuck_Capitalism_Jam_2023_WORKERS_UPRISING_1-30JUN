47646920cb7fce4f5876d69d05a70fd6670f11dc
Nathan Shapter <shapternathan@protonmail.com>
2023-05-31 19:32:13 -0400

    security camera shoots out multiple rays at target looking for player

--- Assets/SecurityCamera.cs
+++ Assets/SecurityCamera.cs
@@ -1,37 +1,76 @@
+using System.Globalization;
 using UnityEngine;
 
 public class SecurityCamera : MonoBehaviour
 {
-    private Ray2D ray;
-    private float maxRayDistance = 10f;
+    private Ray2D[] rays;
+    [SerializeField] float maxRayDistance = 10f;
+    [SerializeField] float verticalOffset = 15f;
+    [SerializeField] float offsetMultiplier = 5f;
 
     [SerializeField] Transform target;
 
+    int numberOfRays = 5;
+    private void Start()
+    {
+        rays = new Ray2D[5];
+        
+    }
     private void Update()
     {
-        ShootRay();
+        ShootRays();
     }
 
-    private void ShootRay()
+    private void ShootRays()
     {
-        ray = new Ray2D(transform.position, target.transform.position - transform.position);
-        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, maxRayDistance);
 
-        if (hit.collider != null)
+       
+
+
+
+
+        Vector2 direction = (target.transform.position- transform.position);
+        Vector2[] directions = new Vector2[]
         {
-            if (hit.collider.CompareTag("Player"))
+            direction,
+            direction + Vector2.up * verticalOffset,
+            direction - Vector2.up * verticalOffset,
+            direction + Vector2.up * verticalOffset *offsetMultiplier,
+            direction + Vector2.up * verticalOffset *offsetMultiplier
+        };
+
+
+        for (int i = 0; i < rays.Length; i++)
+        {
+            rays[i] = new Ray2D(transform.position, directions[i]);
+            RaycastHit2D hit = Physics2D.Raycast(rays[i].origin, rays[i].direction, maxRayDistance);
+            if (hit.collider != null)
             {
-                Debug.Log("Player");
+                if (hit.collider.CompareTag("Player"))
+                {
+                    Debug.Log("Player");
+                }
             }
+
         }
+
+      
+
+      
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(ray.origin, (target.transform.position - transform.position) * maxRayDistance);
-        Gizmos.color = Color.black;
-        
-        
+        if(Application.isPlaying )
+        {
+            for (int i = 0; i < rays.Length; i++)
+            {
+                Gizmos.DrawRay(rays[i].origin, rays[i].direction * maxRayDistance);
+            }
+        }
+       
+
+
     }
 }
