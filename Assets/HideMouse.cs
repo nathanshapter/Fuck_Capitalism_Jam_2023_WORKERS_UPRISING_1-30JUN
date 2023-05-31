d22fbd4ef64cd19842e54578709229d5e7d449b5
Nathan Shapter <shapternathan@protonmail.com>
2023-05-31 16:56:11 -0400

    added crosshair for mouse, locked mouse to ingame

---
+++ Assets/HideMouse.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideMouse : MonoBehaviour
+{
+    private void Start()
+    {
+     
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+}
